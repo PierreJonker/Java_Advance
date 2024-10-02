01017c9d3da931d5698894ab50f232c0b07d3336
Pierre <jonkerpierre637@gmail.com>
2024-10-02 12:55:42 +0200

    Dummies Book 5 Done

---
+++ My_Projects/Book_5/Chapter_1/Sync/DoTwoThings.java
@@ -0,0 +1,19 @@
+package Chapter_1.Sync;
+
+import Chapter_1.CountDownV1.CountDownClock;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+public class DoTwoThings
+{
+    ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(2);
+    CountDownClock clock = new CountDownClock();
+
+    DoTwoThings()
+    {
+        pool.execute(clock);
+        pool.execute(clock);
+        pool.shutdown();
+    }
+
+}
---
+++ My_Projects/Book_5/Chapter_3/RecursiveFactorial.java
@@ -0,0 +1,6 @@
+package Chapter_3;
+
+public class RecursiveFactorial
+{
+
+}
